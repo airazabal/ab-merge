76c683e0c8ae4d3c5deb4ced4c68ce2f023a51ae
airazabal <irazabal.alex@gmail.com>
2021-09-15 11:45:55 -0400

    added ssl properties

--- src/main/java/io/confluent/developer/MergeStreams.java
+++ src/main/java/io/confluent/developer/MergeStreams.java
@@ -92,8 +92,8 @@ public class MergeStreams {
         allProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, allProps.getProperty("ssl.truststore.password"));
      
         // for RBAC
-        //allProps.put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
-        //allProps.put(SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO, allProps.getProperty("basic.auth.user.info"));
+        allProps.put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
+        allProps.put(SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO, allProps.getProperty("basic.auth.user.info"));
         //allProps.put(METADATA_SERVER_URL, allProps.getProperty("metadata.server.url"));
         //allProps.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, allProps.getProperty("ssl.key.truststore.type"));
          // add replication factor
